efd63ed74621797a857a048cd53c979cac4db965
Caahmos <dev.alexmorales@gmail.com>
2024-04-06 16:27:15 -0300

    Temas da Aplicação

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,6 +5,7 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        color: ${props => props.theme.colors.white}
     }
 
     html, border-style, #root{
